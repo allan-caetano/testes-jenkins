6f4fdb7381b787bd4677b7e1decba3d964f25c2f
allan-caetano <93058927+allan-caetano@users.noreply.github.com>
2022-09-02 17:15:36 -0300

    done

--- src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -60,7 +60,7 @@ class GeradorDePagamentoTest {
 		
 		Assert.assertEquals(data.plusDays(1),
 				pagamento.getVencimento());
-		Assert.assertEquals(vencedor.getValor(), pagamento.getValor());
+		Assert.assertEquals(vencedor.getValor(), pagamento.getUsuario());
 		Assert.assertFalse(pagamento.getPago());
 		Assert.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
 		Assert.assertEquals(leilao, pagamento.getLeilao());
